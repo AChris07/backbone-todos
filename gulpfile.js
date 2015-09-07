8d36f8b9e00343689e46c9d8da890de17a3d8203
Cristiano Amici <cristiano.amici@admios-sa.com>
2015-09-07 11:41:42 -0500

    Include SCSS for theming, related changes to markup and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,26 @@ var gulp = require('gulp');
 var amdOptimize = require('amd-optimize');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
 gulp.task('compress', function() {
   return gulp.src('js/**/*.js')
     .pipe(amdOptimize("main"))
     .pipe(concat('app.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('sass', function() {
+  gulp.src('scss/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('watch', function() {
   gulp.watch('js/**/*.js', ['compress']);
+  gulp.watch('scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['compress', 'watch'], function() {
+gulp.task('default', ['compress', 'sass', 'watch'], function() {
 });
